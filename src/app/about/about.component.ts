a5aa452f77e154a46a6fb71cdfdd78f12f433df2
Jacob Ollila <jake.ollila@clearsurance.com>
2019-01-09 20:03:58 -0500

    Added skeleton code for about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public aboutItems = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.aboutItems = [
+      {
+        "img": "assets/design.png",
+        "title": "Design",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+          "Aliquam elementum ex eu porttitor molestie. Donec luctus ante nec ex " +
+          "faucibus volutpat. Integer nec commodo quam. Vestibulum ex lorem, " +
+          "auctor eu consectetur sed."
+      },
+      {
+        "img": "assets/code.png",
+        "title": "Development",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+          "Aliquam elementum ex eu porttitor molestie. Donec luctus ante nec ex " +
+          "faucibus volutpat. Integer nec commodo quam. Vestibulum ex lorem, " +
+          "auctor eu consectetur sed."
+      },
+      {
+        "img": "assets/cloud.png",
+        "title": "Hosting",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+          "Aliquam elementum ex eu porttitor molestie. Donec luctus ante nec ex " +
+          "faucibus volutpat. Integer nec commodo quam. Vestibulum ex lorem, " +
+          "auctor eu consectetur sed."
+      }
+    ]
   }
 
 }
