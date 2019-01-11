1cd55cbc508ef60e8626e79c9f3e50e75a17b5c5
Jacob Ollila <jake.ollila@clearsurance.com>
2019-01-10 20:40:19 -0500

    Start of projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  public projects = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.projects = [
+      {
+        "name": "Jumla",
+        "image": "assets/jumla.png",
+        "description": "Jumla is a personal project that I developed. It aggregates data from " +
+          "multiple social media outlets including Twitter and Instagram as well as from news " +
+          "sources and centralizes the data into a single feed.",
+        "link": "https://jakeollila.space"
+      }
+    ]
   }
 
 }
