21e5132bc74bc74201165ac988b7fe35fe943c60
Ollila <ollilaj@wit.edu>
2019-11-10 12:46:51 -0500

    Updated npm packages and Jumla link

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -20,7 +20,7 @@ export class ProjectsComponent implements OnInit {
         "image": "assets/jumla-collage.png",
         "description": "Jumla is the ultimate place to keep up to date with your favorite celebrities. " +
           "Explore recent news and their social media prescence in this all-encompassing web app.",
-        "link": "http://jumla.link"
+        "link": "https://github.com/ollilaj/Jumla"
       }
     ]
   }
